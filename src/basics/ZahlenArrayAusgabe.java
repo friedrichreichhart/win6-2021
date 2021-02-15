f774913f663cd976ef098d234a40ddbfa3b58627
rei <rei@AMS-GYM-EDV1-01.gym.ams.local>
2021-02-15 15:12:11 +0100

    BubbleSort zahlenArray

---
+++ src/basics/ZahlenArrayAusgabe.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class ZahlenArrayAusgabe {
+
+	public static void main(String[] args) {
+		int[] zahlen = {3, 2, 6, 9, 1, 2, 8, 5, 3, 1};
+		//
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+	}
+
+}
